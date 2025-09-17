11d6908e0dee51ffcdc107b3648fc975c04eabfd
dsamithmendis <samithmendis.01@gmail.com>
2025-09-17 05:41:27 +0530

    dev: build foundation

--- src/lib/archviz-data.ts
+++ src/lib/archviz-data.ts
@@ -1,8 +1,8 @@
 import { ArchvizItem } from "@/types/archviz.types";
 
 export const ArchvizItems: ArchvizItem[] = [
-  { title: "Luv", image: "/images/film/1.jpg" },
-  { title: "the clocks", image: "/images/film/2.jpg" },
-  { title: "WEDYE", image: "/images/film/3.jpg" },
-  { title: "HOST", image: "/images/film/4.jpg" },
+  { title: "Luv", image: "/images/film/1.webp" },
+  { title: "the clocks", image: "/images/film/2.webp" },
+  { title: "WEDYE", image: "/images/film/3.webp" },
+  { title: "HOST", image: "/images/film/4.webp" },
 ];
--- src/lib/sketch-data.ts
+++ src/lib/sketch-data.ts
@@ -1,13 +1,13 @@
 import { SketchItem } from "@/types/sketch.types";
 
 export const SketchItems: SketchItem[] = [
-  { title: "", image: "/images/sketch/9.jpg" },
-  { title: "", image: "/images/sketch/8.jpg" },
-  { title: "", image: "/images/sketch/3.jpg" },
-  { title: "", image: "/images/sketch/2.jpg" },
-  { title: "", image: "/images/sketch/5.jpg" },
-  { title: "", image: "/images/sketch/6.jpg" },
-  { title: "", image: "/images/sketch/7.jpg" },
-  { title: "", image: "/images/sketch/4.jpg" },
-  { title: "", image: "/images/sketch/1.jpg" },
+  { title: "", image: "/images/sketch/9.webp" },
+  { title: "", image: "/images/sketch/8.webp" },
+  { title: "", image: "/images/sketch/3.webp" },
+  { title: "", image: "/images/sketch/2.webp" },
+  { title: "", image: "/images/sketch/5.webp" },
+  { title: "", image: "/images/sketch/6.webp" },
+  { title: "", image: "/images/sketch/7.webp" },
+  { title: "", image: "/images/sketch/4.webp" },
+  { title: "", image: "/images/sketch/1.webp" },
 ];
